782f50712242e74277f5a82b78e44aeda7f12dcc
Uttam Marandi <uttammarandi007@gmail.com>
2021-07-08 20:10:00 +0530

    color flipper simple complete

--- 01-Color-Flipper/app.js
+++ 01-Color-Flipper/app.js
@@ -1,10 +1,20 @@
 //1. colors[] have the color values. Get button and span tag as js variables.
+// 2 . Onclick - generate a random number between 0 & 3 as colors[] has 4 element 
+//3. change the style(background color) of body
+//4.  display the color name on span tag same to the background
+//5. refactor code : instead of hard coded colors[] length...use array.length
+
 
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 
 const btn = document.getElementById("btn")
 const color = document.querySelector(".color")
 
+
 btn.addEventListener("click" , function(){
-    console.log(document.body);
+    // let randomNumber = Math.floor(Math.random() * 4) //2
+    let randomNumber = Math.floor(Math.random() * colors.length) //5
+    document.body.style.backgroundColor = colors[randomNumber] //3
+    color.textContent = colors[randomNumber]
+    
 })
