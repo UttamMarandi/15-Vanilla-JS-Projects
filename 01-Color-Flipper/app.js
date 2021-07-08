973c1f39912e26c2e23772ac02984170c0bbac42
Uttam Marandi <uttammarandi007@gmail.com>
2021-07-08 19:46:18 +0530

    html complete

---
+++ 01-Color-Flipper/app.js
@@ -0,0 +1,10 @@
+//1. colors[] have the color values. Get button and span tag as js variables.
+
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById("btn")
+const color = document.querySelector(".color")
+
+btn.addEventListener("click" , function(){
+    console.log(document.body);
+})
