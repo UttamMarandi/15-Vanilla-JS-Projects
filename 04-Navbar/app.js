05aab09e990b2a1d94eff838148caa812666dd82
Uttam Marandi <uttammarandi007@gmail.com>
2021-07-10 23:29:28 +0530

    navbar toggle complete

--- 04-Navbar/app.js
+++ 04-Navbar/app.js
@@ -4,16 +4,22 @@
 // remove - remove class
 // toggle - toggles class
 
-const navToggle = document.querySelector(".nav-toggle")
+//1. select all required elements : nav-toggle and links
+//2. eventlistener for navtoggle that removes or adds the show links class to show / hide the nav bar
+//3. Use the element.classList.toggle method to toggle navbar
+
+const navToggle = document.querySelector(".nav-toggle") //1
 const links = document.querySelector(".links")
 console.log(links);
 
-navToggle.addEventListener("click" , function() {
+navToggle.addEventListener("click" , function() { 
     
-    if(links.classList.contains("show-links")) {
-        links.classList.remove("show-links")
-    }
-    else {
-        links.classList.add("show-links")
-    }
+    // if(links.classList.contains("show-links")) { //2
+    //     links.classList.remove("show-links")
+    // }
+    // else {
+    //     links.classList.add("show-links")
+    // }
+
+    links.classList.toggle("show-links")
 })
