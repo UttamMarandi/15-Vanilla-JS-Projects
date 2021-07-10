f10aebdbdd10ef215e8af0e4dc54ea0a292e6068
Uttam Marandi <uttammarandi007@gmail.com>
2021-07-10 23:48:29 +0530

    understand classList, contains , classList.remove() , add() , toggle()

---
+++ 05-Sidebar/app.js
@@ -0,0 +1,13 @@
+// the code is similar to previou one. - 04 Navbar toggle
+//1. select required elements - siderbar, nav toggle button, nav close button
+//2. use element.classList.toggle on navToggle (eventlistener used)
+//3. use element.classList.remove on navClose.
+
+const sidebarToggle = document.querySelector(".sidebar-toggle")
+const sidebar = document.querySelector(".sidebar")
+const btnClose = document.querySelector(".close-btn")
+
+sidebarToggle.addEventListener("click" , function() {
+    sidebar.classList.toggle("show-sidebar")
+})
+
