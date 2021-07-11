1840a7c3a12c57e31a4b0f030f3a5e3865a6d427
Uttam Marandi <uttammarandi007@gmail.com>
2021-07-11 23:52:24 +0530

    08- Menu Project start - html done - understand data

--- 07-Questions Tab/app-selectors.js
+++ 07-Questions Tab/app-selectors.js
@@ -31,34 +31,3 @@ questions.forEach(function(question) { //2
                
     })
 })
-
-
-// This prject can be completed in tewo ways
-
-//1. using selectors inside the element
-
-// 1 . select all the question-btn element
-//2. use forEach() method to acess the clicked button 
-//3. use e.currentTarget.parenelement to access 'questions' div of the clicked button 
-//4. add "show text" class to the questions div 
-//5. when one of the question text is open ..remove "show-text " class from other questions. for that access 'question' div and use forEach on that 
-
-const btns = document.querySelectorAll(".question-btn") //1
-const question = document.querySelectorAll(".question")
-
-
-btns.forEach(function(btn){ //2
-    btn.addEventListener("click" , function(e) {
-        console.log(e.currentTarget.parentElement.parentElement); //3
-        
-        question.forEach (function(ques){ //5
-            ques.classList.remove("show-text")
-        })
-
-        e.currentTarget.parentElement.parentElement.classList.toggle("show-text") //4
-
-    })
-})
-
-
-// traversing the dom
