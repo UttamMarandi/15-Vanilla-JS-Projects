86bdb33aa8f0d7a9a033e2fcf43f8dcb017e090f
Uttam Marandi <uttammarandi007@gmail.com>
2021-07-13 19:57:11 +0530

    Video project complete - preloader add

--- 09-Video/app.js
+++ 09-Video/app.js
@@ -6,13 +6,20 @@
 //2. add an click event to button . 
 //3. slide class moves the span with class switch to left 50 %. Add the "slide" class to btn and remove it if already added to create click functionality in ui
 
+//Pre-Loader
+//4. Add a "load" event on window . diffrence between load and DOMContentLoaded
+//5. add "hide=preloader" class when load is fired
 
-const btn = document.querySelector(".switch-btn")
+
+const btn = document.querySelector(".switch-btn")//1
 const video = document.querySelector(".video-container")
+const preloader = document.querySelector(".preloader")
 
-btn.addEventListener("click" , function(){
+
+
+btn.addEventListener("click" , function(){ //2
    
-    if(!btn.classList.contains("slide")) {
+    if(!btn.classList.contains("slide")) { //3
         btn.classList.add("slide")
         video.pause()
     }
@@ -22,3 +29,6 @@ btn.addEventListener("click" , function(){
     }
 })
 
+window.addEventListener("load",function(){ //4
+    preloader.classList.add("hide-preloader") //5
+})
