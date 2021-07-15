a9b917d9649514110a1ae0959e2f2584f784d690
Uttam Marandi <uttammarandi007@gmail.com>
2021-07-15 13:54:52 +0530

    12-Countdown timer complete - clearInterval()

--- 12-Countdown-Timer/app.js
+++ 12-Countdown-Timer/app.js
@@ -15,6 +15,10 @@
 //12. format the output so that if value in remain is less than 10 than an extra 0 before it
 //13. use for each to access each items i.e h4 in deadline-format and pass single item and index to it's callback function
 //14. set the innerhtml of h4 to the remain[] values
+//15. if time remaining is less than 0 means that countdown has experied. change innerHTML of ".deadline " accordingly
+//16. use setInterval() to call getRemainingtime once each second
+//17. also clearInterval() when checking for time remaining < 0, so that we stop getInterval.
+//18. call getRemainingTime() in the end so that we have access to clearInterval() after time remaining < 0
 const months = [
   "January",
   "February",
@@ -43,7 +47,7 @@ const giveway = document.querySelector(".giveway")//2
 const deadline = document.querySelector(".deadline")
 const items = document.querySelectorAll(".deadline-format h4") //innovative
 
-let futureDate = new Date(2021 , 6 , 24, 18, 34) //3
+let futureDate = new Date(2021 , 6 , 15, 13, 43) //3
 console.log(futureDate);
 
 const year = futureDate.getFullYear()//4
@@ -60,9 +64,9 @@ giveway.textContent = `giveway ends on ${weekday} , ${date} ${months[month]} ${y
 
 
 function getRemainingTime() {
-  let futureTime = futureDate.getTime() //returns value in ms
+  let futureTime = futureDate.getTime() //returns value in ms 8
   let today = new Date().getTime()
-  let t = futureTime - today
+  let t = futureTime - today //9
   console.log(t);
   //1s = 1000ms
   //1m = 60s
@@ -77,27 +81,33 @@ function getRemainingTime() {
   let dayRemain =Math.floor( t/oneDay)
   // let hourRemain = Math.floor((t-(dayRemain * oneDay))/oneHour)
   //above works but tedious..use modulus operartor instead ..stable and scalabele
-  let hourRemain = Math.floor((t % oneDay)/oneHour)
+  let hourRemain = Math.floor((t % oneDay)/oneHour) //10
   console.log(hourRemain);
   let minRemain = Math.floor((t % oneHour )/oneMinute)
   console.log(minRemain);
   let secsRemain = Math.floor((t % oneMinute) / oneSecs)
   console.log(secsRemain);
 
-  let remain = [dayRemain , hourRemain, minRemain, secsRemain ]
+  let remain = [dayRemain , hourRemain, minRemain, secsRemain ] //11
 
-  function format(iformat) {
+  function format(iformat) { //12
     if(iformat < 10) {
       return `0 ${iformat}`
     }
     return iformat
   }
 
-  items.forEach(function(item, index){ //passing single item as well as index as parameter
-    item.innerHTML = format(remain[index])
+  items.forEach(function(item, index){ //passing single item as well as index as parameter 13
+    item.innerHTML = format(remain[index]) //14
   })
   
+  if(t<0) { //15
+    clearInterval(countdown) //17
+    deadline.innerHTML = `<h4 class="expired"> Sorry, this giveway has expired </h4>`
+  }
+
 }
 //countdown
-let countdown = setInterval (getRemainingTime, 1000)
-getRemainingTime()
+let countdown = setInterval (getRemainingTime, 1000) //16
+  
+getRemainingTime() //18
